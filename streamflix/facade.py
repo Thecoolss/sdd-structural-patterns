be5733f429aa18fc2383a98ceae9bcacbedc58af
Sami Droubi <sami.t.droubi@gmail.com>
2026-09-17 17:13:57 +0200

    Add streaming facade

--- streamflix/facade.py
+++ streamflix/facade.py
@@ -11,14 +11,18 @@ class StreamingFacade:
 
     def __init__(self, payment_processor: PaymentProcessor):
       # TODO: store the payment processor and start unsubscribed
-      pass
+      self.payment_processor = payment_processor
+      self.status="unsubscribed"
 
     def subscribe(self, monthly_fee: float) -> str:
       # TODO: charge `monthly_fee` through the payment processor, mark the
       # account as subscribed, and return the processor's receipt string.
-      pass
+      self.status="subscribed"
+      return self.payment_processor.pay(monthly_fee)
 
     def watch(self, video: Video) -> str:
       # TODO: if not subscribed, raise PermissionError("subscription required").
       # Otherwise delegate to `video.play()` and return its result.
-      pass
+      if self.status != "subscribed":
+          raise PermissionError("subscription required")
+      return video.play()
